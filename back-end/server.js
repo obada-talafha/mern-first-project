bd2782916bb32c2dc17b53427db2b9cbeb258be1
Obada Talafha <obada.talafha20@gmail.com>
2025-11-06 15:27:01 +0300

    created routes and controller

--- back-end/server.js
+++ back-end/server.js
@@ -1,35 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/Product.model.js";
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // allow to accept json data in body
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
-  if (!product.name || !product.price || !product.image) {
-    return res.status(400).json({
-      success: false,
-      message: "please provide all required fields",
-    });
-  }
-  const newProduct = new Product(product);
-  try {
-    await newProduct.save();
-    res.status(201).json({
-      success: true,
-      data: newProduct,
-    });
-  } catch (error) {
-    console.error("Error in create product:", error.message);
-    res.status(500).json({
-      success: false,
-      message: "Server Error",
-    });
-  }
-});
+
+app.use("/api/products", productRoutes);
+
 console.log(process.env.MONGO_URI);
+
 app.listen(5000, () => {
   connectDB();
   console.log("Server is running on port 5000 Hello");
