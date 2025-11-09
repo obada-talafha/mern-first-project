611a65185c546de6bf82018442dd916aac97986d
Obada Talafha <obada.talafha20@gmail.com>
2025-11-09 12:23:36 +0300

    feat: implement CreatePage component for adding new products

--- front-end/src/pages/CreatePage.jsx
+++ front-end/src/pages/CreatePage.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-
+import React, { use } from 'react'
+import { Box, Button, Container, Heading, Input, VStack, useColorModeValue } from '@chakra-ui/react';
 const CreatePage = () => {
+    const [newProduct, setNewProduct] = React.useState({
+        name: '',
+        price: '',
+        imageUrl: ''
+    });
+
+    const handleAddProduct = () => {
+        console.log(newProduct);
+    }
+
     return (
-        <div>
-            Create Page
-        </div>
+        <Container maxW={'container.sm'}>
+            <VStack spacing={8} >
+                <Heading as="h1" size="2xl" textAlign={'center'} mb={8}>
+                    Create New Product
+                </Heading>
+                <Box
+                    w={'full'} bg={useColorModeValue('white', 'gray.800')}
+                    p={6} rounded={'lg'} shadow={'md'}
+                >
+                    <VStack spacing={4}>
+                        <Input
+                            name="name"
+                            placeholder="Product Name"
+                            value={newProduct.name}
+                            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                        />
+                        <Input
+                            name="price"
+                            placeholder="Product Price"
+                            value={newProduct.price}
+                            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                        />
+                        <Input
+                            name="imageUrl"
+                            placeholder="Image URL"
+                            value={newProduct.imageUrl}
+                            onChange={(e) => setNewProduct({ ...newProduct, imageUrl: e.target.value })}
+                        />
+                        <Button colorScheme="blue"
+                            onClick={handleAddProduct} w={'full'}
+                        >
+                            Create Product
+                        </Button>
+                    </VStack>
+                </Box>
+            </VStack>
+        </Container>
     )
 }
 
